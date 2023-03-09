f2da76e3ed653813a1c3c66d8335afd6eea0dbdd
damisumida <gjptmda628@gmail.com>
2023-03-09 09:23:06 +0900

    各処理をメソッドに切り出し

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,34 +1,58 @@
-let now = new Date()
-let year = now.getFullYear()
-let month = now.getMonth()
+function disp_calendar(){
+  const argv = require('minimist')(process.argv.slice(2));
+  const now = new Date()
+  const year = parse_year(argv, now)
+  const month = parse_month(argv, now)
+  const first_day = new Date(year, month, 2)
+  const last_day = new Date(year, month + 1, 0)
 
-const argv = require('minimist')(process.argv.slice(2));
-if(argv.y != undefined){
-  year = argv.y
+  console.log((month + 1 + "月 " + year).padStart(13))
+  console.log("日 月 火 水 木 金 土")
+  disp_first_week(first_day)
+  disp_nextweeks(first_day, last_day)
 }
-if(argv.m != undefined){
-  month = argv.m - 1
-}
 
-let first_day = new Date(year, month, 2)
-let last_day = new Date(year, month + 1, 0)
-
-console.log((month+1 + "月 " + year).padStart(13))
-console.log("日 月 火 水 木 金 土")
-let first_week = ''
-for (let i = 1; i <= 8-first_day.getDay(); i++){
-  first_week = first_week.concat(String(i).padStart(2))
-  first_week = first_week.concat(' ')
-  if(first_day.getDay() == 0){ break; }
-}
-console.log(first_week.padStart(21))
-
-for (let i = 0; i < 4; i++){
-  let week = ''
-  for(let j = 1; j < 8; j++){
-    if (last_day.getDate() < 8-first_day.getDay() + j + i * 7){ break; }
-    week = week.concat(String(8-first_day.getDay() + j + i * 7).padStart(2))
-    week = week.concat(' ')
+function parse_year(argv, now){
+  if(argv.y != undefined){
+    return argv.y
+  } else {
+    return now.getFullYear()
   }
-  console.log(week)
 }
+
+function parse_month(argv, now){
+  if(argv.m != undefined){
+    return argv.m - 1
+  } else {
+    return now.getMonth()
+  }
+}
+
+function disp_first_week(first_day){
+  const first_week = build_first_week(first_day)
+  console.log(first_week.padStart(20))
+}
+
+function build_first_week(first_day){
+  let week = ''
+  for (let i = 1; i <= 8-first_day.getDay(); i++){
+    week = week.concat(String(i).padStart(2))
+    week = week.concat(' ')
+    if(first_day.getDay() == 0){ break; }
+  }
+  return week
+}
+
+function disp_nextweeks(first_day, last_day){
+  for (let i = 0; i < 4; i++){
+    let week = ''
+    for(let j = 1; j < 8; j++){
+      if (last_day.getDate() < 8-first_day.getDay() + j + i * 7){ break; }
+      week = week.concat(String(8-first_day.getDay() + j + i * 7).padStart(2))
+      week = week.concat(' ')
+    }
+    console.log(week)
+  }
+}
+
+disp_calendar()
