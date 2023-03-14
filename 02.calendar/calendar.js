cc60dd3b7a2b69b8ba359de4c37f65be396933c3
damisumida <gjptmda628@gmail.com>
2023-03-14 17:14:51 +0900

    メソッド名を修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,4 +1,4 @@
-function disp_calendar(){
+function display_calendar(){
   const argv = require('minimist')(process.argv.slice(2));
   const now = new Date()
   const year = (argv.y != undefined) ? argv.y : now.getFullYear()
@@ -8,21 +8,21 @@ function disp_calendar(){
 
   console.log((month + 1 + "月 " + year).padStart(13))
   console.log("日 月 火 水 木 金 土")
-  disp_date(first_date, last_date, year, month)
+  display_date(first_date, last_date, year, month)
 }
 
-function disp_date(first_date, last_date, year, month){
+function display_date(first_date, last_date, year, month){
   process.stdout.write('1'.padStart(3 * first_date.getDay() + 2))
-  disp_suffix(first_date)
+  display_suffix(first_date)
   for(let i = 2; i <= last_date.getDate(); i++){
     process.stdout.write(String(i).padStart(2))
     const date = new Date(year, month, i)
-    disp_suffix(date)
+    display_suffix(date)
   }
   process.stdout.write('\n')
 }
 
-function disp_suffix(date){
+function display_suffix(date){
   if(date.getDay() == 6){
     process.stdout.write('\n')
   } else {
@@ -30,4 +30,4 @@ function disp_suffix(date){
   }
 }
 
-disp_calendar()
+display_calendar()
