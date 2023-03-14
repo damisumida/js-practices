094307379e71c52ee57e60a110d3a0ec07ac9c27
damisumida <gjptmda628@gmail.com>
2023-03-14 17:11:08 +0900

    カレンダーの改行方法を修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -3,39 +3,30 @@ function disp_calendar(){
   const now = new Date()
   const year = (argv.y != undefined) ? argv.y : now.getFullYear()
   const month = (argv.m != undefined) ? argv.m - 1 : now.getMonth()
-  const first_date = new Date(year, month, 2)
+  const first_date = new Date(year, month, 1)
   const last_date = new Date(year, month + 1, 0)
 
   console.log((month + 1 + "月 " + year).padStart(13))
   console.log("日 月 火 水 木 金 土")
-  disp_first_week(first_date)
-  disp_nextweeks(first_date, last_date)
+  disp_date(first_date, last_date, year, month)
 }
 
-function disp_first_week(first_day){
-  const first_week = build_first_week(first_day)
-  console.log(first_week.padStart(20))
-}
-
-function build_first_week(first_day){
-  let week = ''
-  for (let i = 1; i <= 8-first_day.getDay(); i++){
-    week = week.concat(String(i).padStart(2))
-    if(first_day.getDay() == 0){ break; }
-    week = week.concat(' ')
+function disp_date(first_date, last_date, year, month){
+  process.stdout.write('1'.padStart(3 * first_date.getDay() + 2))
+  disp_suffix(first_date)
+  for(let i = 2; i <= last_date.getDate(); i++){
+    process.stdout.write(String(i).padStart(2))
+    const date = new Date(year, month, i)
+    disp_suffix(date)
   }
-  return week
+  process.stdout.write('\n')
 }
 
-function disp_nextweeks(first_day, last_day){
-  for (let i = 0; i < 4; i++){
-    let week = ''
-    for(let j = 1; j < 8; j++){
-      if (last_day.getDate() < 8-first_day.getDay() + j + i * 7){ break; }
-      week = week.concat(String(8-first_day.getDay() + j + i * 7).padStart(2))
-      week = week.concat(' ')
-    }
-    console.log(week)
+function disp_suffix(date){
+  if(date.getDay() == 6){
+    process.stdout.write('\n')
+  } else {
+    process.stdout.write(' ')
   }
 }
 
